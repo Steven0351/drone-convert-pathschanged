4c8e9795fd1a10eb1585dc4b6f9010a7ae937e84
Steven Sherry <steven.sherry@affinityforapps.com>
2020-10-11 00:17:57 -0500

    Add relveant files that were missed in initial commit. Add whitespace between default pipeline and arm64 pipeline separator

---
+++ scripts/build_arm.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# disable go modules
+export GOPATH=""
+
+# disable cgo
+export CGO_ENABLED=0
+
+set -e
+set -x
+
+# linux
+GOOS=linux GOARCH=arm64 go build -o release/linux/arm64/drone-convert-pathschanged
